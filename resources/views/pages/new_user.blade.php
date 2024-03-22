bd28b559cff1ea603d77548a812727fc3a0eacc9
LAPTOP-JAUKE\jauke <jauke_1991@live.nl>
2024-03-22 11:49:54 +0100

    some styling to the user form

--- resources/views/pages/new_user.blade.php
+++ resources/views/pages/new_user.blade.php
@@ -13,7 +13,7 @@
         </div>
     @endif
     <h1 class="text-white text-7xl pl-16 pb-14 pt-6">Hacklab</h1>
-    <form class="bg-gray-900/50 ml-16 border-none block w-5/12 h-4/6" name="create_user_form" method="POST"
+    <form class="bg-gray-900/50 ml-16 border-none block w-5/12 h-6/6" name="create_user_form" method="POST"
         action="{{ route('store_user') }}">
         <h2 class="text-white text-3xl pl-4 pt-10">Add new user</h2><br>
         @csrf
@@ -36,35 +36,35 @@
             type="password" name="password" id="Password" placeholder="Password" required>
 
         <input
-            class="bg-gray-500/50 text-white focus:ring-1 focus:outline-none p-3 my-12 block w-96 m-4
+            class="bg-gray-500/50 text-white focus:ring-1 focus:outline-none p-3 mt-12 mb-6 block w-96 m-4
             border-2 border-gray-400 focus:border-none rounded-sm focus:ring-hacklab_green focus:outline-1
             outline-hacklab_green placeholder-gray-400 focus:placeholder-hacklab_green"
             type="tel" name="phone_number" id="phone_number" placeholder="Phone number">
 
-        <label class="text-white m-4">Date of Birth
+        <label class="text-white ml-4 mb-0">Date of Birth
             <input
-                class="bg-gray-500/50 text-white focus:ring-1 focus:outline-none p-3 my-10 block w-96 m-4
+                class="bg-gray-500/50 text-white focus:ring-1 focus:outline-none p-3 mt-0 mb-12 block w-96 m-4
             border-2 border-gray-400 focus:border-none rounded-sm focus:ring-hacklab_green focus:outline-1
             outline-hacklab_green placeholder-gray-400 focus:placeholder-hacklab_green"
                 type="date" name="date_of_birth" id="date_of_birth" placeholder="Date of birth" required>
         </label>
 
         <input
-            class="bg-gray-500/50 text-white focus:ring-1 focus:outline-none p-3 my-12 block w-96 m-4
+            class="bg-gray-500/50 text-white focus:ring-1 focus:outline-none p-3 mt-0 mb-6 block w-96 m-4
             border-2 border-gray-400 focus:border-none rounded-sm focus:ring-hacklab_green focus:outline-1
             outline-hacklab_green placeholder-gray-400 focus:placeholder-hacklab_green"
             type="text" name="hometown" id="hometown" placeholder="Home town" required>
 
-        <label class="text-white m-4">Start date
+        <label class="text-white ml-4 mb-0">Start date
             <input
-                class="bg-gray-500/50 text-white focus:ring-1 focus:outline-none p-3 my-10 block w-96 m-4
+                class="bg-gray-500/50 text-white focus:ring-1 focus:outline-none p-3 mt-0 mb-12 block w-96 m-4
             border-2 border-gray-400 focus:border-none rounded-sm focus:ring-hacklab_green focus:outline-1
             outline-hacklab_green placeholder-gray-400 focus:placeholder-hacklab_green"
                 type="date" name="start_date" id="start_date" placeholder="Start date" required>
         </label>
 
         <button
-            class="bg-hacklab_green border-none rounded-lg w-40 py-3 block m-4 tranistion ease-in-out
+            class="bg-hacklab_green border-none rounded-lg w-40 py-3 ml-52 block tranistion ease-in-out
             delay-150 duration-200 hover:bg-sky-400 hover:text-white"
             type="submit">Register</button>
     </form>
