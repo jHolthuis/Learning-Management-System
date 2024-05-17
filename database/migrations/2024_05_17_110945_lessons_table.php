0bbbd790379c646b568a54af75fa2f0becbb49aa
LAPTOP-JAUKE\jauke <jauke_1991@live.nl>
2024-05-17 11:30:53 +0200

    ran some migrations to be able to make a schedule

---
+++ database/migrations/2024_05_17_110945_lessons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->foreignId('subject_id');
+            $table->foreignId('user_id');
+            $table->foreignId('classroom_id');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->string('day_of_week');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
