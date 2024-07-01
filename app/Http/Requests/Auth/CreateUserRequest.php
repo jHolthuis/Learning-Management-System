c6fe991fbf5bea8e940087f57e15a1ebcc0a2faa
jHolthuis <jauke_1991@live.nl>
2024-07-01 11:21:37 +0200

    added requests and other essential pages

---
+++ app/Http/Requests/Auth/CreateUserRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+
+        return [
+                'name' => 'required|string',
+                'email' => 'required|email:filter|unique:users,password',
+                'password' => 'required',
+                'phone_number' => 'required',
+                'date_of_birth' => 'date',
+                'hometown' => 'string|required',
+                'start_date' =>  'date',
+                'role_id' => 'required|exists:roles,id',
+                'loan_laptop' => 'boolean',
+            ];
+    }
+}
