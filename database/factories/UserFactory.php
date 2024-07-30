452213756551f3b23776714e638a08c176e83e05
jHolthuis <jauke_1991@live.nl>
2024-07-30 15:18:30 +0200

    some changes to the factory and tests.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,9 +5,13 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
  */
 class UserFactory extends Factory
 {
@@ -28,14 +32,14 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'phone_number' => fake()->unique()->phoneNumber(),
+            'phone_number' => fake()->unique()->numerify('6########'),
             'date_of_birth' => fake()->date(),
             'home_town' => fake()->city(),
             'start_date' => fake()->date(),
             'remember_token' => Str::random(10),
-            'role_id' => '4',
-            'loan_laptop' => '0',
-            'is_admin' => '1',
+            'role_id' => Role::factory(),
+            'loan_laptop' => 0,
+            'is_admin' => 1,
         ];
     }
 
--- tests/Feature/UserListTest.php
+++ tests/Feature/UserListTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Role;
 
 class UserListTest extends TestCase
 {
@@ -15,13 +16,13 @@ class UserListTest extends TestCase
      */
     public function test_example(): void
     {
-
-        $admin = User::factory()->create();
+        $role = Role::factory()->has(User::factory())->make();
+        $admin = User::factory()->for($role)->make();
 
         $response = $this->actingAs($admin)
                     ->withSession(['banned' => false])
                     ->get('/');
 
-        $response;
+        $response->assertStatus(200);
     }
 }
